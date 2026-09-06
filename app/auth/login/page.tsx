1b92a4ba9c203aef7ac631b62b69e78035fab02e
LINCONL AUGUSTO SOARES ARAUJO <linconl1997@gmail.com>
2026-09-06 12:32:43 +0200

    auth pages

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,16 @@
+import { LoginForm } from "@/components/auth/login-form"
+import Navbar from "@/components/navbar"
+
+export default function LoginPage() {
+  return (
+    <main className="min-h-screen bg-background text-foreground">
+          <Navbar />
+        
+        <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+          <div className="w-full max-w-sm md:max-w-4xl">
+            <LoginForm />
+          </div>
+        </div>
+        </main>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,12 @@
 import Link from "next/link";
-import { BadgeSwissFranc } from 'lucide-react'; 
 import { Button } from "@/components/ui/button";
+import Navbar from "@/components/navbar";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background text-foreground">
-      <header className="mx-auto flex max-w-7xl items-center justify-between px-6 py-6">
-        <Link href="#inicio" className="flex items-center gap-2 text-xl font-bold"> 
-        <span className="flex h-9 w-9 items-center justify-center rounded-xl bg-primary text-primary-foreground">
-          <BadgeSwissFranc className="h-5 w-5" />
-        </span>
-         Fin<span className="relative -ml-2 -top-1 text-primary">+</span>
-         </Link>
-        <nav className="hidden gap-8 text-sm text-muted-foreground md:flex">
-          <Link href="#inicio">Início</Link>
-          <Link href="#recursos">Recursos</Link>
-          <Link href="#como-funciona">Como funciona</Link>
-          <Link href="#seguranca">Segurança</Link>
-        </nav>
-        {/* Mobile menu button */}
-        <button className="md:hidden">
-          <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
-        <div className=" gap-2 md:flex">
-          <Button variant="link"><Link href="#comecar">Login</Link></Button>
-          <Button variant="default"><Link href="#comecar">Começar agora</Link></Button>
-        </div>
-      </header>
-
+      <Navbar />
       <section id="inicio" className="mx-auto grid max-w-7xl items-center gap-14 px-6 py-16 lg:grid-cols-2 lg:py-28">
         <div>
           <p className="mb-6 inline-flex items-center gap-2 rounded-full border border-border bg-muted px-3 py-1.5 text-xs font-medium text-muted-foreground">
