cee9edb17e9c6c0f87979d7b4114271968b1f03a
LINCONL AUGUSTO SOARES ARAUJO <linconl1997@gmail.com>
2026-09-06 21:41:45 +0200

    small changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-export function dashboard() {
+export default function Dashboard() {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <h1 className="text-2xl font-bold">Dashboard</h1>
