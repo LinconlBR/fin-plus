e66e5cacb2ea878540d4d3d13180cc06d2497a52
LINCONL AUGUSTO SOARES ARAUJO <linconl1997@gmail.com>
2026-09-06 13:03:00 +0200

    middleware changes

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-import { updateSession } from "@/lib/supabase/middleware";
-
-export async function middleware(request: Request) {
-    const response = await updateSession(request as any);
-
-    return response;
-}
