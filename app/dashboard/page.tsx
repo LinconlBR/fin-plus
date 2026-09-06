ec66d4a84825dee011752183394185378a81f5b3
LINCONL AUGUSTO SOARES ARAUJO <linconl1997@gmail.com>
2026-09-06 21:56:06 +0200

    update dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,19 @@
-export default function Dashboard() {
+import { createClient } from "@/lib/supabase/server"
+
+export default async function Dashboard() {
+    const supabase = await createClient()
+    const { data: { user } } = await supabase.auth.getUser()
+    const { data: profile } = await supabase
+     .from("profiles")
+     .select("*")
+     .eq("id", user?.id)
+     .single()
+
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <h1 className="text-2xl font-bold">Dashboard</h1>
       <p className="text-sm text-muted-foreground">
-        Bem-vindo ao seu painel de controle!
+        Bem-vindo ao seu painel de controle , <span className="font-bold">{profile?.full_name}</span>!
       </p>
     </div>
   )
