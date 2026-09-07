accedbe41731cb32d49d9533c20810188b2a0b54
LINCONL AUGUSTO SOARES ARAUJO <linconl1997@gmail.com>
2026-09-07 21:38:18 +0200

    Update login-form.tsx

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -79,7 +79,7 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form formaction={formAction} className="p-6 md:p-8">
+          <form action={formAction} className="p-6 md:p-8">
             <FieldGroup>
               <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Que bom ter você de volta</h1>
