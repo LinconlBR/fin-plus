d127779b001cf7a0ab8cd3d1c57ce5d4ad4c5f84
LINCONL AUGUSTO SOARES ARAUJO <linconl1997@gmail.com>
2026-09-08 01:23:05 +0200

    update dashboard
    implementando chart and card sections on dashboard
    - install rechart

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -19,7 +19,6 @@ export default async function Layout({ children }: { children: React.ReactNode }
   return (
     <SidebarProvider>
       <AppSidebar user={{ name: profile?.full_name ?? "", email: user?.email ?? "", avatar: "/avatar.svg"}} />
-      <main>
         <SidebarInset>
             <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
                 <div className="flex items-center gap-2 px-4">
@@ -33,7 +32,6 @@ export default async function Layout({ children }: { children: React.ReactNode }
             </header>
         {children}
         </SidebarInset>
-      </main>
     </SidebarProvider>
 
     
