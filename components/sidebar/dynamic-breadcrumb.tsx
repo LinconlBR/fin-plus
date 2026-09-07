226b0c42ccdbc2ee7b8ca4a0f29a85b1021ff9d7
LINCONL AUGUSTO SOARES ARAUJO <linconl1997@gmail.com>
2026-09-07 13:17:50 +0200

    Update dynamic-breadcrumb.tsx

--- components/sidebar/dynamic-breadcrumb.tsx
+++ components/sidebar/dynamic-breadcrumb.tsx
@@ -7,22 +7,43 @@ import {
     BreadcrumbLink,
     BreadcrumbList,
     BreadcrumbPage,
+    BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
 
 export default function DynamicBreadcrumb() {
   const pathname = usePathname()
-console.log (pathname)
+
+
   return (
     <Breadcrumb>
-                        <BreadcrumbList>
-                            
-                            <BreadcrumbItem className="hidden md:block">
-                                <BreadcrumbLink href="#">
-                                    <BreadcrumbPage>{pathname.split("/")}</BreadcrumbPage>
-                                </BreadcrumbLink>
-                            </BreadcrumbItem>
-                        </BreadcrumbList>
-                    </Breadcrumb>
+        <BreadcrumbList>
+        {pathname.split("/").filter(Boolean) .map((segment, index, array) => {
+            if (index === array.length - 1) {
+                return (
+                    <BreadcrumbItem key={segment}>
+                        <BreadcrumbPage>
+                            {segment}
+                        </BreadcrumbPage>
+                    </BreadcrumbItem>
+                )
+            }
+            return (  
+                <>
+                <BreadcrumbItem key="hidden md:block">
+                    <BreadcrumbLink href={`/${segment}`}>
+                        {segment}
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+             </>)
+        })}
+            <BreadcrumbItem key="">
+                <BreadcrumbLink href="#">
+                    
+                </BreadcrumbLink>
+            </BreadcrumbItem>
+        </BreadcrumbList>
+    </Breadcrumb>
   )
 
 }
