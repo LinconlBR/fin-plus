a7e0606e33b64f770299e3442fdcad5428bb47e6
LINCONL AUGUSTO SOARES ARAUJO <linconl1997@gmail.com>
2026-09-08 02:04:37 +0200

    chart fix errors

--- components/ui/chart-interactive.tsx
+++ components/ui/chart-interactive.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+import {useState, useEffect } from "react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import {
@@ -142,14 +143,7 @@ const chartConfig = {
 
 export function ChartAreaInteractive() {
   const isMobile = useIsMobile()
-  const [timeRange, setTimeRange] = React.useState("90d")
-
-  
-  React.useEffect(() => {
-    if (isMobile) {
-      setTimeRange("7d")
-    }
-  }, [isMobile])
+  const [timeRange, setTimeRange] = useState(isMobile ? "7d" : "90d")
 
   const filteredData = chartData.filter((item) => {
     const date = new Date(item.date)
@@ -171,15 +165,21 @@ export function ChartAreaInteractive() {
         <CardTitle>Total Visitors</CardTitle>
         <CardDescription>
           <span className="hidden @[540px]/card:block">
-            Total for the last 3 months
+            {timeRange === "90d" && "Total for the last 3 months"}
+            {timeRange === "30d" && "Total for the last 30 days"}
+            {timeRange === "7d" && "Total for the last 7 days"}
           </span>
+
+
           <span className="@[540px]/card:hidden">Last 3 months</span>
         </CardDescription>
         <CardAction>
+            {/**/}
           <ToggleGroup
-            type="single"
-            value={timeRange}
-            onValueChange={setTimeRange}
+            value={[timeRange]}
+            onValueChange={(value) => {
+              if (value && value.length > 0) setTimeRange(value[0])
+            }}
             variant="outline"
             className="hidden *:data-[slot=toggle-group-item]:px-4! @[767px]/card:flex"
           >
@@ -187,9 +187,15 @@ export function ChartAreaInteractive() {
             <ToggleGroupItem value="30d">Last 30 days</ToggleGroupItem>
             <ToggleGroupItem value="7d">Last 7 days</ToggleGroupItem>
           </ToggleGroup>
-          <Select value={timeRange} onValueChange={setTimeRange}>
+            
+          <Select
+            value={timeRange}
+            onValueChange={(value) => setTimeRange(value ?? "90d")}
+          >
             <SelectTrigger
+
               className="flex w-40 **:data-[slot=select-value]:block **:data-[slot=select-value]:truncate @[767px]/card:hidden"
+             // className="flex w-40 **:data-[slot=select-value]:block **:data-[slot=select-value]:truncate"
               size="sm"
               aria-label="Select a value"
             >
@@ -212,7 +218,7 @@ export function ChartAreaInteractive() {
       <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
         <ChartContainer
           config={chartConfig}
-          className="aspect-auto h-[250px] w-full"
+          className="aspect-auto h-62.5 w-full"
         >
           <AreaChart data={filteredData}>
             <defs>
