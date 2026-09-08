e08eeba152c92dcf4d27243ff8fec5d365b99f38
LINCONL AUGUSTO SOARES ARAUJO <linconl1997@gmail.com>
2026-09-08 10:59:42 +0200

    cards update

--- app/(app)/dashboard/page.tsx
+++ app/(app)/dashboard/page.tsx
@@ -4,11 +4,39 @@ import { ChartAreaInteractive } from "@/components/dashboard/chart-interactive"
 
 export default async function Dashboard() {
 
+interface CardData {
+  label: string
+  value: string
+  trend: "up" | "down"
+  trendLabel: string
+  description: string
+  comment?: string
+}
+
+const cardData: CardData[] = [
+  {
+    label: "Ganhos",
+    value: " R$ 1500",
+    trend: "up",
+    trendLabel: "10%",
+    description:  "+ R$ 200 em relação ao mês anterior",
+    comment: "Ganhos do mês",
+  },
+  {
+    label: "Despesas",
+    value: " R$ 600",
+    trend: "down",
+    trendLabel: " 33%",
+    description:  "- R$ 300 em relação ao mês anterior",
+    comment: "despesas do mês",
+  },
+]
+
   return (
     <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
+              <SectionCards  cards={cardData}  />
               <div className="px-4 lg:px-6">
                 <ChartAreaInteractive /> 
               </div>
--- components/dashboard/section-cards.tsx
+++ components/dashboard/section-cards.tsx
@@ -10,54 +10,22 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-type CardData = {
+
+
+
+interface CardData {
   label: string
   value: string
   trend: "up" | "down"
   trendLabel: string
   description: string
+  comment?: string
 }
 
-const cardData: CardData[] = [
-  {
-    label: "string",
-    value: "",
-    trend: "up",
-    trendLabel: "string",
-    description:  "string",
-  },
-]
-
-
-
-
-export function SectionCards() {
+export function SectionCards({ cards }: { cards: CardData[] }) {
   return (
     <div className="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Total Revenue</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-            $1,250.00
-          </CardTitle>
-          <CardAction>
-            <Badge variant="outline">
-              <TrendingUp />
-              +12.5%
-            </Badge>
-          </CardAction>
-        </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium">
-            Trending up this month <TrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Visitors for the last 6 months
-          </div>
-        </CardFooter>
-      </Card>
-      
-      {cardData.map((card, index) => (
+      {cards.map((card, index) => (
         <Card key={index} className="@container/card">
           <CardHeader>
             <CardDescription>{card.label}</CardDescription>
@@ -73,15 +41,10 @@ export function SectionCards() {
           </CardHeader>
           <CardFooter className="flex-col items-start gap-1.5 text-sm">
             <div className="line-clamp-1 flex gap-2 font-medium">
-              {card.description}{" "}
-              {card.trend === "up" ? (
-                <TrendingUp className="size-4" />
-              ) : (
-                <TrendingDown className="size-4" />
-              )}
+              {card.description}
             </div>
             <div className="text-muted-foreground">
-              Visitors for the last 6 months
+              {card.comment ? card.comment : "Sem comentários"}
             </div>
           </CardFooter>
         </Card>
