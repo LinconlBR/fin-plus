c183b08667a8d20cfa3da036f0a296fa830dd6e9
LINCONL AUGUSTO SOARES ARAUJO <linconl1997@gmail.com>
2026-09-09 09:45:32 +0200

    removendo antiga pagina

--- app/(app)/transactions/page.tsx
+++ app/(app)/transactions/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import { DataTable } from "@/components/ui/data-table";
-import data from "./data.json"
+
 
 export default function TransactionsPage() {
-	
-	return (
-		<main className="min-h-screen bg-slate-50 px-4 py-8 text-slate-900 sm:px-8">
-			<DataTable data={data} /> 
-		</main>
-	);
+  return <div>Em construção</div>
 }
