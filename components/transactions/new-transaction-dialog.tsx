6a873c117621cf98c6c0d0b5ef2388dd851e1d36
LINCONL AUGUSTO SOARES ARAUJO <linconl1997@gmail.com>
2026-09-11 20:35:39 +0200

    Update new-transaction-dialog.tsx

--- components/transactions/new-transaction-dialog.tsx
+++ components/transactions/new-transaction-dialog.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+
 import { useState } from "react"
 import {
   Dialog,
@@ -10,8 +11,28 @@ import {
 } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { transactionSchema } from "@/lib/schema/transactions"
+import { z } from "zod"
+
+type TransactionFormValues = z.infer<typeof transactionSchema>
+
+
+
 export function NewTransactionDialog() {
   const [open, setOpen] = useState(false)
+
+  const form = useForm<TransactionFormValues>({
+  resolver: zodResolver(transactionSchema),
+  defaultValues: {
+    description: "",
+    amount: 0,
+    type: "expense",
+    category_id: "",
+    date: new Date().toISOString().split("T")[0],
+  },
+})
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
